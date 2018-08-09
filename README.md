c8176f604eb189e27ee19dbe014c461fe6304ccb
WYX <wyx@WYXdeMacBook-Pro.local>
2018-08-09 14:12:24 +0800

    feat:  generator-coding

---
+++ README.md
@@ -0,0 +1,17 @@
+# generator-coding
+generator-coding  javascript 练习算法的脚手架
+
+#### 使用方式
+
+------
+
+`` npm install -g yo generator-coding ``
+
+`` yo coding ``
+
+* 初始化后脚手架，进入创建好的目录下依次执行 `` npm install ``   `` npm run start ``
+* 完成后在目录下的 index.js 下开始你的 js 算法编程之旅
+* 脚手架目录参考本项目 app/templates/
+* 欢迎提 issue
+
+* __支付宝招前端 or 安卓，校招 or 社招。社招 P6 起，欢迎简历来砸。__
---
+++ app/templates/lib/io.js
@@ -0,0 +1,50 @@
+import readline from 'readline';
+import { hint } from '../dist/util';
+
+const options = {
+  ioFlag: true                // 此处 hack 掉循环时关闭流之后的输入数据提示
+}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const checkout = (arr) => {
+  var index = arr.indexOf('');
+      if(index > -1){
+        arr.splice(index,1);
+        checkout(arr)
+      }
+        return arr;
+}
+
+const readWrap = (max,type) => {
+  return new Promise((resolve, reject) => {
+    rl.on('line', (input) => {
+      if(typeof max == 'undefined'){
+        resolve(input);
+      }else{
+        var inputData = input.split(' ');
+        inputData = checkout(inputData);
+        if(inputData.length == max){
+          var result = typeof type == 'undefined' ? inputData.join(' ') : inputData;
+          resolve(result);
+        }
+        else
+        console.log('请输入'+max+'个数据');
+      }
+    });
+  }) 
+}
+
+export const read = async (max,type) => {
+  if (!options.ioFlag) return ;
+  hint.warn('请输入数据: ')
+  return await readWrap(max,type);
+}
+
+export const print = (msg) => {
+  hint.success('运行结果: ', msg)
+  rl.close();
+  options.ioFlag = false;
+}
